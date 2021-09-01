3c04351b3d4589d4cbc84d95c3bd79ed3a676b45
Daniel Fenner <daniel.fenner@ruhr-uni-bochum.de>
2021-09-01 23:35:47 +0200

    Added copyright and warranty statement to source files

--- R/cqcp_filter.R
+++ R/cqcp_filter.R
@@ -1,3 +1,19 @@
+# CrowdQC+ - Quality control for citizen weather station data.
+# Copyright (C) 2021  Daniel Fenner, Tom Grassmann, Benjamin Bechtel, Matthias Demuzere, Jonas Kittner, Fred Meier
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #' Main QC step m1
 #'
 #' Flag values with FALSE that are missing or belong to a p_id having lon & lat
